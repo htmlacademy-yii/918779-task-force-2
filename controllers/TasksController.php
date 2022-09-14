f825767b4d05e16d5eb6454e9b05a61254b25ca5
Anton Romanov <609990@gmail.com>
2022-09-14 07:37:20 +0300

    Правки

--- controllers/TasksController.php
+++ controllers/TasksController.php
@@ -5,14 +5,8 @@ namespace app\controllers;
 use Yii;
 
 use yii\web\Controller;
-
-use app\models\Task;
-use app\models\Category;
-use app\models\City;
 use app\models\TaskFilterForm;
-use app\models\Response;
-
-use yii\db\Expression;
+use app\models\Category;
 use yii\web\NotFoundHttpException;
 
 class TasksController extends Controller {
@@ -21,42 +15,25 @@ class TasksController extends Controller {
 
         $filter = new TaskFilterForm();
 
-        $task = Task::find()
-        ->where(['status' => Task::STATUS_NEW])
-        ->joinWith(['category', 'city', 'responses'])
-        ->orderBy(['creation' => SORT_DESC]);
+        $tasks = $filter->getTasks()->all();
 
         $request = Yii::$app->request->getIsPost();
 
         if ($request) {
 
             $filter->load(Yii::$app->request->post());
-            $selected_categories = count($filter->categories);
 
             if ($filter->validate()) {
 
-                if  ($selected_categories > 0) {
-                    $task->andWhere(['in', 'category_id', $filter->categories]);
-                }
-
-                if ($filter->remoteWork) {
-                    $task->andWhere(['city_id' => null]);
-                }
-
-                if ($filter->noResponse) {
-                    $task->andWhere(['task_id' => null]);
-                }
+               $tasks = $filter->apply();
 
             }
         }
 
-        $tasks = $task->all();
-        $categories = Category::find()->all();
-
         return $this->render('index', [
             'tasks' => $tasks,
             'filter' => $filter,
-            'categories' => $categories,
+            'categories' => TaskFilterForm::getCategories(),
             'period_values' => TaskFilterForm::PERIOD_VALUES
         ]);
     }
--- models/TaskFilterForm.php
+++ models/TaskFilterForm.php
@@ -5,6 +5,11 @@ namespace app\models;
 use Yii;
 use yii\base\Model;
 use app\models\Category;
+use app\models\Task;
+use app\models\City;
+use app\models\Response;
+use yii\db\ActiveQuery;
+use yii\db\Expression;
 
 class TaskFilterForm extends \yii\db\ActiveRecord {
 
@@ -20,26 +25,10 @@ class TaskFilterForm extends \yii\db\ActiveRecord {
         self::PERIOD_DAY => '24 часа'
     ];
 
-
     public $categories = [];
     public $remoteWork;
     public $noResponse;
     public $period;
-
-    public function getPeriod($period) {
-        settype($period, 'integer');
-
-        switch($period) {
-            case self::PERIOD_HOUR:
-                return $task->andWhere(['=', 'creation', 'DATE_SUB(NOW(), INTERVAL self::PERIOD_HOUR HOUR)']);
-
-            case self::PERIOD_HALF_DAY:
-                return $task->andWhere(['=', 'creation', 'DATE_SUB(NOW(), INTERVAL self::PERIOD_HALF_DAY HOUR)']);
-
-            case self::PERIOD_DAY:
-                return $task->andWhere(['=', 'creation', 'DATE_SUB(NOW(), INTERVAL self::PERIOD_DAY HOUR)']);
-        };
-    }
 
     public const NO_RESPONSE = 'Без откликов';
 
@@ -57,5 +46,69 @@ class TaskFilterForm extends \yii\db\ActiveRecord {
             ['remoteWork', 'boolean', 'trueValue' => true, 'falseValue' => false, 'strict' => true],
             ['noResponse', 'boolean', 'trueValue' => true, 'falseValue' => false, 'strict' => true],
         ];
+    }
+
+    public function getTasks(): ActiveQuery
+    {
+        $tasks = Task::find()
+        ->where(['status' => Task::STATUS_NEW])
+        ->joinWith(['category', 'city', 'responses'])
+        ->orderBy(['creation' => SORT_DESC]);
+
+        return $tasks;
+    }
+
+    private function getPeriod($tasks): ActiveQuery
+    {
+
+        settype($this->period, 'integer');
+
+        switch($this->period) {
+            case self::PERIOD_HOUR:
+                return $tasks->andWhere(['=', 'creation', "DATE_SUB(NOW(), INTERVAL self::PERIOD_HOUR HOUR)"]);
+
+            case self::PERIOD_HALF_DAY:
+                return $tasks->andWhere(['=', 'creation', "DATE_SUB(NOW(), INTERVAL self::PERIOD_HALF_DAY HOUR)"]);
+
+            case self::PERIOD_DAY:
+                return $tasks->andWhere(['=', 'creation', "DATE_SUB(NOW(), INTERVAL self::PERIOD_DAY HOUR)"]);
+        };
+
+    }
+
+    public static function getCategories(): array
+    {
+
+        $categories = Category::find()->all();
+
+        return $categories;
+    }
+
+    public function apply(): array
+    {
+
+        $tasks = $this->getTasks();
+
+        $selected_categories = count($this->categories);
+
+        if  ($selected_categories > 0) {
+            $tasks->andWhere(['in', 'category_id', $this->categories]);
+        }
+
+        if ($this->remoteWork) {
+            $tasks->andWhere(['city_id' => null]);
+        }
+
+        if ($this->noResponse) {
+            $tasks->andWhere(['task_id' => null]);
+        }
+
+        if ($this->period) {
+            $this->getPeriod($tasks);
+        }
+
+        $tasks->orderBy(['creation' => SORT_ASC]);
+
+        return $tasks()->all;
     }
 }
